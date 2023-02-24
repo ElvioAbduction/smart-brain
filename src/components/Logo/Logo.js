5a7ec09102461ffb2db759b60f24654c6e1a2978
ElvioAbduction <109813828+ElvioAbduction@users.noreply.github.com>
2023-02-24 15:57:19 +0100

    third commit

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tilt from 'react-parallax-tilt';
 import './Logo.css';
+import transparentLogo from './logo_transparent.png';
 
 
 const Logo = () => {
+ 
+	const [scale, setScale] = useState(1.15);
 	return(
 		<div className=' ma4 mt0 '>
-			<Tilt className="logo br2 shadow-2">
-      			<div>👀</div>
+			<Tilt className="logo tilt-scale br2 shadow-2" scale={scale} transitionSpeed={2500}>
+      			<div><img src={transparentLogo} alt='Smart Brain'/></div>
     		</Tilt>
 		</div>
 		)
